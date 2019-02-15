b86581096a1cda0778674aa59f4d6076dea68077
ipzelhakim <iip@sleekr.co>
2019-02-15 09:35:00 +0700

    fungsional halaman karir, belum beres utk upload file nya

--- www/tricomindo/po-content/component/karir/karir.php
+++ www/tricomindo/po-content/component/karir/karir.php
@@ -3,7 +3,7 @@
  *
  * - PopojiCMS Front End File
  *
- * - File : contact.php
+ * - File : karir.php
  * - Version : 1.0
  * - Author : Jenuar Dalapang
  * - License : MIT License
@@ -25,48 +25,97 @@ $router->match('GET|POST', '/karir', function() use ($core, $templates) {
 	$lang = $core->setlang('contact', WEB_LANG);
 	if (!empty($_POST)) {
 		$core->poval->validation_rules(array(
-			'contact_name' => 'required|max_len,100|min_len,3',
-			'contact_email' => 'required|valid_email',
-			'contact_subject' => 'required|max_len,255|min_len,6',
-			'contact_message' => 'required|min_len,10'
+			'career_name' => 'required|max_len,100|min_len,3',
+			'career_position' => 'required|max_len,255|min_len,5',
+			'career_message' => 'required|min_len,5'
 		));
 		$core->poval->filter_rules(array(
-			'contact_name' => 'trim|sanitize_string',
-			'contact_email' => 'trim|sanitize_string',
-			'contact_subject' => 'trim|sanitize_email',
-			'contact_message' => 'trim|sanitize_string'
+			'career_name' => 'trim|sanitize_string',
+			'career_position' => 'trim|sanitize_string',
+			'career_message' => 'trim|sanitize_string'
 		));
 		$validated_data = $core->poval->run($_POST);
-		if ($validated_data === false) {
+		
+		$validateFile = true;
+		$fileErrorMsg = '';
+		
+        if (!isset($_FILES['career_attachment']) || empty($_FILES['career_attachment']['tmp_name'])) {
+			$validateFile = false;
+			$fileErrorMsg = "Mungkin ukuran file yang anda upload melebihi ukuran maksimal yang diperkenankan";
+		}
+		
+		if (
+			(!isset($_FILES['career_attachment']['error']) ||
+			is_array($_FILES['career_attachment']['error'])) && $validateFile
+		) {
+			// return false;
+			$validateFile = false;
+			$fileErrorMsg = "Terjadi kesalahan saat mengupload file";
+		}
+		// You should also check filesize here. 
+		if ($_FILES['career_attachment']['size'] > 2000000 && $validateFile) {
+			$validateFile = false;
+			$fileErrorMsg = "Ukuran file melebihi batas maksimum 2MB";
+		}
+		
+		// DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+		// Check MIME Type by yourself.
+		$finfo = new finfo(FILEINFO_MIME_TYPE);
+		if ($validateFile && false === $ext = array_search(
+			$finfo->file($_FILES['career_attachment']['tmp_name']),
+			array(
+				'pdf' => 'application/pdf',
+				'doc' => 'application/msword',
+				'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+			),
+			true
+		)) {
+			$validateFile = false;
+			$fileErrorMsg = "Format file tidak sesuai, harus berupa pdf, doc, atau docx";
+		}
+		
+		if ($validated_data === false || $validateFile === false) {
+			$errorMsg = !empty($fileErrorMsg) ? $fileErrorMsg : $lang['front_career_error'];
 			$alertmsg = '<div id="contact-form-result">
 				<div class="alert alert-warning" role="alert">
 					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					'.$lang['front_contact_error'].'
+					'. $errorMsg .'
 				</div>
 			</div>';
 		} else {
+			$fileData = file_get_contents($_FILES['career_attachment']['tmp_name']);
+			$fileData = base64_encode($fileData);
 			$data = array(
-				'name' => $_POST['contact_name'],
-				'email' => $_POST['contact_email'],
-				'subject' => $_POST['contact_subject'],
-				'message' => $_POST['contact_message']
+				'career_name' => $_POST['career_name'],
+				'career_position' => $_POST['career_position'],
+				'career_message' => $_POST['career_message']
+				// ,'career_attachment' => $fileData
 			);
-			$query = $core->podb->insertInto('contact')->values($data);
-			$query->execute();
+			
+			$msg = '';
+			try {
+				$query = $core->podb->insertInto('career')->values($data);
+				$core->podb->getPdo()->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+				$execute = $query->execute();
+			} catch (\Throwable $th) {
+				$msg = $th->getMessage();
+			}
+
 			unset($_POST);
 			$alertmsg = '<div id="contact-form-result">
 				<div class="alert alert-success" role="alert">
 					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					'.$lang['front_contact_success'].'
+					'.$lang['front_career_success'].'
 				</div>
 			</div>';
 		}
 	}
 	$info = array(
-		'page_title' => $lang['front_contact'],
+		'page_title' => $lang['front_career'],
 		'page_desc' => $core->posetting[2]['value'],
 		'page_key' => $core->posetting[3]['value'],
-		'social_mod' => $lang['front_contact'],
+		'social_mod' => $lang['front_career'],
 		'social_name' => $core->posetting[0]['value'],
 		'social_url' => $core->posetting[1]['value'],
 		'social_title' => $core->posetting[0]['value'],
@@ -75,6 +124,7 @@ $router->match('GET|POST', '/karir', function() use ($core, $templates) {
 		'alertmsg' => $alertmsg
 	);
 	$adddata = array_merge($info, $lang);
+	
 	$templates->addData(
 		$adddata
 	);
--- www/tricomindo/po-content/themes/chingsy/karir.php
+++ www/tricomindo/po-content/themes/chingsy/karir.php
@@ -4,7 +4,7 @@
 	<div class="container clearfix">
 		<ol class="breadcrumb">
 			<li><a href="<?=BASE_URL;?>"><?=$this->e($front_home);?></a></li>
-			<li><a href="<?=BASE_URL.'/contact';?>"><?=$this->e($front_contact);?></a></li>
+			<li><a href="<?=BASE_URL.'/karir';?>"><?=$this->e($front_karir);?></a></li>
 		</ol>
 	</div>
 </section>
@@ -40,26 +40,26 @@
 		<div class="container clearfix">
 			<div class="postcontent nobottommargin">
 				<?=htmlspecialchars_decode($this->e($alertmsg));?>
-				<form class="nobottommargin" id="template-contactform" name="template-contactform" action="<?=BASE_URL;?>/contact" method="post">
+				<form class="nobottommargin" id="template-contactform" name="template-contactform" action="<?=BASE_URL;?>/karir" method="post" enctype="multipart/form-data">
 					<div class="col_half">
 						<label for="template-contactform-name"><?=$this->e($contact_name);?> <small>*</small></label>
-						<input type="text" id="template-contactform-name" name="contact_name" value="<?=(isset($_POST['contact_name']) ? $_POST['contact_name'] : '');?>" class="sm-form-control required" />
+						<input type="text" id="template-contactform-name" name="career_name" value="<?=(isset($_POST['contact_name']) ? $_POST['contact_name'] : '');?>" class="sm-form-control required" />
 					</div>
 					<div class="col_half col_last">
 						<label for="template-contactform-subject">Posisi yang diinginkan <small>*</small></label>
-						<input type="text" id="template-contactform-subject" name="contact_subject" value="<?=(isset($_POST['contact_subject']) ? $_POST['contact_subject'] : '');?>" class="required sm-form-control" />
+						<input type="text" id="template-contactform-subject" name="career_position" value="<?=(isset($_POST['contact_subject']) ? $_POST['contact_subject'] : '');?>" class="required sm-form-control" />
 					</div>
 					<div class="clear"></div>
 					<div class="col_full">
 						<label for="template-contactform-message"><?=$this->e($contact_message);?> <small>*</small></label>
-						<textarea class="required sm-form-control" id="template-contactform-message" name="contact_message" rows="6" cols="30"><?=(isset($_POST['contact_message']) ? $_POST['contact_message'] : '');?></textarea>
+						<textarea class="required sm-form-control" id="template-contactform-message" name="career_message" rows="6" cols="30"><?=(isset($_POST['contact_message']) ? $_POST['contact_message'] : '');?></textarea>
 					</div>
 					<div class="col_full hidden">
 						<input type="text" id="template-contactform-botcheck" name="template-contactform-botcheck" value="" class="sm-form-control" />
 					</div>
 					<div class="col_full">
 						<label for="template-contactform-subject">Upload CV <small>*</small></label>
-						<input type="file" id="cv" name="cv" class="required sm-form-control">
+						<input type="file" id="career_attachment" name="career_attachment" class="required sm-form-control" accept=".doc, .docx, .pdf">
 					</div>
 					<div class="col_full">
 						<div class="g-recaptcha" data-sitekey="<?=$this->pocore()->call->posetting[21]['value'];?>"></div>
